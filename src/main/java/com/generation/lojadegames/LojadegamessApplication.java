09602d28db58ab76aaa4f9db6435530596780630
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-04-02 16:12:39 -0300

    main

---
+++ src/main/java/com/generation/lojadegames/LojadegamessApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojadegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadegamessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadegamessApplication.class, args);
+	}
+
+}
